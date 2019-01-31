02007046c3fc5f50f151adc7ba1ea2cf02791fe1
glebby1 <glebby3@gmail.com>
2019-01-31 09:12:36 +0100

    ajout fc gleb

--- src/FC/Main.java
+++ src/FC/Main.java
@@ -1,4 +1,4 @@
-package Noyau;
+package FC;
 
 public class Main {
     public static void main (String[] args){
---
+++ src/FC/Radiologue.java
@@ -0,0 +1,7 @@
+package FC;
+
+public class Radiologue extends Utilisateur {
+public Radiologue(String nom, String prenom, String id, String mdp){
+    super(nom, prenom,id, mdp);
+}
+}
